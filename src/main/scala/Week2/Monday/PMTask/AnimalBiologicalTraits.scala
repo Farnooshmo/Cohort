ab9db845ab952c6870f717e83af9132a2ce937e7
Farnoosh Moayeri <farnooshmoayeri@gmail.com>
2025-02-11 09:30:05 +0000

    MVP

---
+++ src/main/scala/Week2/Monday/PMTask/AnimalBiologicalTraits.scala
@@ -0,0 +1,6 @@
+package Week2.Monday.PMTask
+
+class AnimalBiologicalTraits
+trait Mammal extends AnimalBiologicalTraits
+trait Bird extends AnimalBiologicalTraits
+trait Insect extends AnimalBiologicalTraits
