062c0896290898a5bb9f17bec459748752f93e8c
Farnoosh Moayeri <farnooshmoayeri@gmail.com>
2025-02-12 10:07:56 +0000

    uncommented file to work on that

--- src/main/scala/Week2/Monday/PMTask/Mammal.scala
+++ src/main/scala/Week2/Monday/PMTask/Mammal.scala
@@ -1,9 +1,9 @@
-//package Week2.Monday.PMTask
-//
-//class Mammal(val furColour: FurColour, val hasTail: HasTail)
-//
-//class FurColour(val colour : String)
-//class HasTail(val hasTail: Boolean)
-//object Mammal {
-//  def apply(furColour: FurColour, hasTail: HasTail): Mammal = new Mammal(furColour,hasTail)
-//}
+package Week2.Monday.PMTask
+
+class Mammal(val furColour: FurColour, val hasTail: HasTail)
+
+class FurColour(val colour : String)
+class HasTail(val hasTail: Boolean)
+object Mammal {
+  def apply(furColour: FurColour, hasTail: HasTail): Mammal = new Mammal(furColour,hasTail)
+}
