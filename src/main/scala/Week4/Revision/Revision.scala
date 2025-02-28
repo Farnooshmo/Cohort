83c707d37df4d5e1a78ece53b14bc8bd42cf61a7
Farnoosh Moayeri <farnooshmoayeri@gmail.com>
2025-02-28 15:06:49 +0000

    creating the md file/week1/Tuesday & create a revision folder

---
+++ src/main/scala/Week4/Revision/Revision.scala
@@ -0,0 +1,14 @@
+package Week4.Revision
+object Revision extends App {
+  //How to calculate the 2 decimal of Pi
+  val pi: Double = 3.141592
+  println(f"Pi to 2 decimal places is $pi%.2f")
+  //
+  val name1: String = "Misha"
+  val name2: String = "Mino"
+  val int1: Int = 1
+  val int2: Int = 3
+
+  print(f"$name2 is ${int2 - int1} years older than $name1")
+}
+//
